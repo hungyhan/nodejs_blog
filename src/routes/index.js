f7b1c2d8bc9f661139c4a5ccd9b949fe2b2790b6
hungyhan <mhybh9597@gmail.com>
2026-03-03 23:18:11 +0700

    add page site

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,12 @@
 const newsRouter = require("./news");
+const stieRouter = require("./site");
 
 function routes(app) {
-  app.get("/", (req, res) => {
-    res.render("home");
-  });
 
-  // app.get("/news", (req, res) => {
-  //   res.render("news");
-  // });
   app.use("/news", newsRouter);
 
-  app.get("/search", (req, res) => {
-    res.render("search");
-  });
+  app.use("/", stieRouter);
 
-  app.post("/search", (req, res) => {
-    console.log("POST /search");
-    console.log(req.body);
-    res.send("");
-  });
 }
 
 module.exports = routes;
